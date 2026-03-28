ac274df71b4667a590bde21f0cefc76c4cd7903d
swjnxyf <1039872007@qq.com>
2026-03-28 23:40:48 +0800

    refactor(xline): remove most tonic dependencies and lightly refactor several components.

--- project/Xline/Xline/crates/xline/src/router/h3wrapper.rs
+++ project/Xline/Xline/crates/xline/src/router/h3wrapper.rs
@@ -11,7 +11,18 @@ use std::task::{Context, Poll};
 
 use anyhow::{Error, anyhow};
 use bytes::{Buf, Bytes};
+use futures::StreamExt;
 use h3::server::RequestStream;
+use http::Request;
+use prost::Message;
+use tokio_stream::Stream;
+use tower::Service;
+use xlinerpc::{
+    MetaData, Request as XlineRequest, Response as XlineResponse, Status as XlineStatus,
+    Streaming as XlineStreaming,
+};
+
+type RpcFuture<T, E> = Pin<Box<dyn Future<Output = Result<T, E>> + Send>>;
 
 /// An incoming HTTP/3 body.
 ///
@@ -130,5 +141,472 @@ impl<S: h3::quic::RecvStream> http_body::Body for QuicIncomingBody<S> {
             State::Data(Some(0)) | State::Trailers | State::Done => true,
             State::Data(_) => false,
         }
+    }
+}
+
+const GRPC_HEADER_SIZE: usize = 5;
+
+fn grpc_ok_response(body: axum::body::Body) -> http::Response<axum::body::Body> {
+    let mut response = http::Response::new(body);
+    let headers = response.headers_mut();
+    let _ = headers.insert(
+        http::header::CONTENT_TYPE,
+        http::HeaderValue::from_static("application/grpc+proto"),
+    );
+    response
+}
+
+fn grpc_error_response(status: XlineStatus) -> http::Response<axum::body::Body> {
+    let mut response = http::Response::new(axum::body::Body::empty());
+    let headers = response.headers_mut();
+    let _ = headers.insert(
+        http::header::CONTENT_TYPE,
+        http::HeaderValue::from_static("application/grpc+proto"),
+    );
+
+    let grpc_code = i32::from(status.code()).to_string();
+    let grpc_status = http::HeaderValue::from_str(&grpc_code)
+        .unwrap_or_else(|_| http::HeaderValue::from_static("2"));
+    let _ = headers.insert(
+        http::header::HeaderName::from_static("grpc-status"),
+        grpc_status,
+    );
+
+    let grpc_message = http::HeaderValue::from_str(status.message())
+        .unwrap_or_else(|_| http::HeaderValue::from_static("rpc error"));
+    let _ = headers.insert(
+        http::header::HeaderName::from_static("grpc-message"),
+        grpc_message,
+    );
+
+    *response.status_mut() = http::StatusCode::OK;
+    response
+}
+
+fn grpc_frame_encode<M: Message>(msg: &M) -> Bytes {
+    let body = msg.encode_to_vec();
+    let len = body.len() as u32;
+    let mut out = Vec::with_capacity(GRPC_HEADER_SIZE + body.len());
+    out.push(0); // uncompressed
+    out.extend_from_slice(&len.to_be_bytes());
+    out.extend_from_slice(&body);
+    Bytes::from(out)
+}
+
+fn grpc_frame_decode<M: Message + Default>(buf: &[u8]) -> Result<(M, usize), XlineStatus> {
+    if buf.len() < GRPC_HEADER_SIZE {
+        return Err(XlineStatus::internal("gRPC frame header too short"));
+    }
+    if buf[0] != 0 {
+        return Err(XlineStatus::internal(
+            "compressed gRPC frames are not supported",
+        ));
+    }
+    let len = u32::from_be_bytes([buf[1], buf[2], buf[3], buf[4]]) as usize;
+    let total = GRPC_HEADER_SIZE + len;
+    if buf.len() < total {
+        return Err(XlineStatus::internal("gRPC frame body too short"));
+    }
+    let msg = M::decode(&buf[GRPC_HEADER_SIZE..total])
+        .map_err(|e| XlineStatus::internal(format!("protobuf decode error: {e}")))?;
+    Ok((msg, total))
+}
+
+fn decode_all_grpc_frames<M: Message + Default>(buf: &[u8]) -> Result<Vec<M>, XlineStatus> {
+    let mut msgs = Vec::new();
+    let mut offset = 0;
+    while offset < buf.len() {
+        let (msg, consumed) = grpc_frame_decode::<M>(&buf[offset..])?;
+        msgs.push(msg);
+        offset += consumed;
+    }
+    Ok(msgs)
+}
+
+fn metadata_from_headers(headers: &http::HeaderMap) -> MetaData {
+    let mut meta = MetaData::new();
+    for (k, v) in headers {
+        if let Ok(vs) = v.to_str() {
+            meta.insert(k.as_str(), vs);
+        }
+    }
+    meta
+}
+
+async fn read_body_bytes<B>(body: B) -> Result<Vec<u8>, XlineStatus>
+where
+    B: http_body::Body<Data = Bytes> + Send + 'static,
+    B::Error: Into<crate::router::Error> + Send + 'static,
+{
+    let mut bytes = Vec::new();
+    let mut body = std::pin::pin!(body);
+
+    while let Some(frame) = futures::future::poll_fn(|cx| body.as_mut().poll_frame(cx)).await {
+        let frame = frame.map_err(|e| XlineStatus::internal(e.into().to_string()))?;
+        if let Ok(data) = frame.into_data() {
+            bytes.extend_from_slice(data.as_ref());
+        }
+    }
+
+    Ok(bytes)
+}
+
+fn spawn_grpc_request_stream<B, M>(body: B) -> XlineStreaming<M>
+where
+    B: http_body::Body<Data = Bytes> + Send + 'static,
+    B::Error: Into<crate::router::Error> + Send + 'static,
+    M: Message + Default + Send + 'static,
+{
+    let (tx, rx) = tokio::sync::mpsc::channel::<Result<M, XlineStatus>>(128);
+
+    let _ = tokio::spawn(async move {
+        let mut body = std::pin::pin!(body);
+        let mut buf = Vec::new();
+
+        while let Some(frame) = futures::future::poll_fn(|cx| body.as_mut().poll_frame(cx)).await {
+            let frame = match frame {
+                Ok(f) => f,
+                Err(e) => {
+                    let _ = tx
+                        .send(Err(XlineStatus::internal(e.into().to_string())))
+                        .await;
+                    return;
+                }
+            };
+
+            if let Ok(data) = frame.into_data() {
+                buf.extend_from_slice(data.as_ref());
+
+                loop {
+                    if buf.len() < GRPC_HEADER_SIZE {
+                        break;
+                    }
+
+                    let len = u32::from_be_bytes([buf[1], buf[2], buf[3], buf[4]]) as usize;
+                    let total = GRPC_HEADER_SIZE + len;
+                    if buf.len() < total {
+                        break;
+                    }
+
+                    let decoded = grpc_frame_decode::<M>(&buf[..total]);
+                    let _ = buf.drain(..total);
+
+                    match decoded {
+                        Ok((msg, _)) => {
+                            if tx.send(Ok(msg)).await.is_err() {
+                                return;
+                            }
+                        }
+                        Err(e) => {
+                            let _ = tx.send(Err(e)).await;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        if !buf.is_empty() {
+            let _ = tx
+                .send(Err(XlineStatus::internal(
+                    "incomplete gRPC frame at end of request stream",
+                )))
+                .await;
+        }
+    });
+
+    XlineStreaming::new(Box::pin(tokio_stream::wrappers::ReceiverStream::new(rx)))
+}
+
+#[derive(Clone)]
+pub(crate) struct MakeUnarySVC<SVC, Input, Output> {
+    inner: SVC,
+    _1: std::marker::PhantomData<Input>,
+    _2: std::marker::PhantomData<Output>,
+}
+
+impl<SVC, Input, Output> MakeUnarySVC<SVC, Input, Output>
+where
+    SVC: Clone,
+{
+    pub(crate) fn new(service: SVC) -> Self {
+        Self {
+            inner: service,
+            _1: std::marker::PhantomData,
+            _2: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<B, SVC, Input, Output> Service<Request<B>> for MakeUnarySVC<SVC, Input, Output>
+where
+    Input: Message + Default + Send + 'static,
+    Output: Message + Default + Send + 'static + Clone,
+    SVC: Service<XlineRequest<Input>, Response = XlineResponse<Output>, Error = XlineStatus>
+        + Clone
+        + 'static
+        + Send
+        + Sync,
+    SVC::Future: Send,
+    B: http_body::Body<Data = Bytes> + Send + 'static,
+    B::Error: Into<crate::router::Error> + Send + 'static,
+{
+    type Response = http::Response<axum::body::Body>;
+    type Error = std::convert::Infallible;
+    type Future = RpcFuture<Self::Response, Self::Error>;
+
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, request: Request<B>) -> Self::Future {
+        let mut svc = self.inner.clone();
+        let fut = async move {
+            let (parts, body) = request.into_parts();
+            let meta = metadata_from_headers(&parts.headers);
+
+            let req_bytes = match read_body_bytes(body).await {
+                Ok(b) => b,
+                Err(e) => return Ok(grpc_error_response(e)),
+            };
+
+            let mut reqs = match decode_all_grpc_frames::<Input>(&req_bytes) {
+                Ok(v) => v,
+                Err(e) => return Ok(grpc_error_response(e)),
+            };
+
+            if reqs.len() != 1 {
+                return Ok(grpc_error_response(XlineStatus::invalid_argument(format!(
+                    "unary request expects exactly 1 frame, got {}",
+                    reqs.len()
+                ))));
+            }
+
+            let rpc_req = XlineRequest::new(reqs.remove(0), meta);
+            match svc.call(rpc_req).await {
+                Ok(resp) => {
+                    let framed = grpc_frame_encode(resp.data());
+                    Ok(grpc_ok_response(axum::body::Body::from(framed)))
+                }
+                Err(e) => Ok(grpc_error_response(e)),
+            }
+        };
+        Box::pin(fut)
+    }
+}
+
+#[derive(Clone)]
+pub(crate) struct MakeStreamingSvc<SVC, Input, Output> {
+    inner: SVC,
+    _1: std::marker::PhantomData<Input>,
+    _2: std::marker::PhantomData<Output>,
+}
+
+impl<SVC, Input, Output> MakeStreamingSvc<SVC, Input, Output>
+where
+    SVC: Clone,
+{
+    pub(crate) fn new(service: SVC) -> Self {
+        Self {
+            inner: service,
+            _1: std::marker::PhantomData,
+            _2: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<B, SVC, Input, Output, RspStream> Service<Request<B>> for MakeStreamingSvc<SVC, Input, Output>
+where
+    Input: Message + Default + Send + 'static,
+    Output: Message + Default + Send + 'static + Clone,
+    RspStream: Stream<Item = Result<Output, XlineStatus>> + Send + 'static,
+    SVC: Service<
+            XlineRequest<XlineStreaming<Input>>,
+            Response = XlineResponse<RspStream>,
+            Error = XlineStatus,
+        >
+        + Clone
+        + 'static
+        + Send
+        + Sync,
+    SVC::Future: Send,
+    B: http_body::Body<Data = Bytes> + Send + 'static,
+    B::Error: Into<crate::router::Error> + Send + 'static,
+{
+    type Response = http::Response<axum::body::Body>;
+    type Error = std::convert::Infallible;
+    type Future = RpcFuture<Self::Response, Self::Error>;
+
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, request: Request<B>) -> Self::Future {
+        let mut svc = self.inner.clone();
+        let fut = async move {
+            let (parts, body) = request.into_parts();
+            let meta = metadata_from_headers(&parts.headers);
+            let input_stream = spawn_grpc_request_stream::<B, Input>(body);
+            let rpc_req = XlineRequest::new(input_stream, meta);
+
+            match svc.call(rpc_req).await {
+                Ok(resp) => {
+                    let out = resp.into_inner().map(|item| {
+                        item.map(|msg| grpc_frame_encode(&msg))
+                            .map_err(|e| std::io::Error::other(e.to_string()))
+                    });
+                    Ok(grpc_ok_response(axum::body::Body::from_stream(out)))
+                }
+                Err(e) => Ok(grpc_error_response(e)),
+            }
+        };
+        Box::pin(fut)
+    }
+}
+
+#[derive(Clone)]
+pub(crate) struct MakeServerStreamingSvc<SVC, Input, Output> {
+    inner: SVC,
+    _1: std::marker::PhantomData<Input>,
+    _2: std::marker::PhantomData<Output>,
+}
+
+impl<SVC, Input, Output> MakeServerStreamingSvc<SVC, Input, Output>
+where
+    SVC: Clone,
+{
+    pub(crate) fn new(service: SVC) -> Self {
+        Self {
+            inner: service,
+            _1: std::marker::PhantomData,
+            _2: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<B, SVC, Input, Output, RspStream> Service<Request<B>>
+    for MakeServerStreamingSvc<SVC, Input, Output>
+where
+    Input: Message + Default + Send + 'static,
+    Output: Message + Default + Send + 'static + Clone,
+    RspStream: Stream<Item = Result<Output, XlineStatus>> + Send + 'static,
+    SVC: Service<XlineRequest<Input>, Response = XlineResponse<RspStream>, Error = XlineStatus>
+        + Clone
+        + 'static
+        + Send
+        + Sync,
+    SVC::Future: Send,
+    B: http_body::Body<Data = Bytes> + Send + 'static,
+    B::Error: Into<crate::router::Error> + Send + 'static,
+{
+    type Response = http::Response<axum::body::Body>;
+    type Error = std::convert::Infallible;
+    type Future = RpcFuture<Self::Response, Self::Error>;
+
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, request: Request<B>) -> Self::Future {
+        let mut svc = self.inner.clone();
+        let fut = async move {
+            let (parts, body) = request.into_parts();
+            let meta = metadata_from_headers(&parts.headers);
+
+            let req_bytes = match read_body_bytes(body).await {
+                Ok(b) => b,
+                Err(e) => return Ok(grpc_error_response(e)),
+            };
+
+            let mut reqs = match decode_all_grpc_frames::<Input>(&req_bytes) {
+                Ok(v) => v,
+                Err(e) => return Ok(grpc_error_response(e)),
+            };
+
+            if reqs.len() != 1 {
+                return Ok(grpc_error_response(XlineStatus::invalid_argument(format!(
+                    "server-streaming request expects exactly 1 frame, got {}",
+                    reqs.len()
+                ))));
+            }
+
+            let rpc_req = XlineRequest::new(reqs.remove(0), meta);
+            match svc.call(rpc_req).await {
+                Ok(resp) => {
+                    let out = resp.into_inner().map(|item| {
+                        item.map(|msg| grpc_frame_encode(&msg))
+                            .map_err(|e| std::io::Error::other(e.to_string()))
+                    });
+                    Ok(grpc_ok_response(axum::body::Body::from_stream(out)))
+                }
+                Err(e) => Ok(grpc_error_response(e)),
+            }
+        };
+        Box::pin(fut)
+    }
+}
+
+#[derive(Clone)]
+pub(crate) struct MakeClientStreamingSvc<SVC, Input, Output> {
+    inner: SVC,
+    _1: std::marker::PhantomData<Input>,
+    _2: std::marker::PhantomData<Output>,
+}
+
+impl<SVC, Input, Output> MakeClientStreamingSvc<SVC, Input, Output>
+where
+    SVC: Clone,
+{
+    pub(crate) fn new(service: SVC) -> Self {
+        Self {
+            inner: service,
+            _1: std::marker::PhantomData,
+            _2: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<B, SVC, Input, Output> Service<Request<B>> for MakeClientStreamingSvc<SVC, Input, Output>
+where
+    Input: Message + Default + Send + 'static,
+    Output: Message + Default + Send + 'static + Clone,
+    SVC: Service<
+            XlineRequest<XlineStreaming<Input>>,
+            Response = XlineResponse<Output>,
+            Error = XlineStatus,
+        >
+        + Clone
+        + 'static
+        + Send
+        + Sync,
+    SVC::Future: Send,
+    B: http_body::Body<Data = Bytes> + Send + 'static,
+    B::Error: Into<crate::router::Error> + Send + 'static,
+{
+    type Response = http::Response<axum::body::Body>;
+    type Error = std::convert::Infallible;
+    type Future = RpcFuture<Self::Response, Self::Error>;
+
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, request: Request<B>) -> Self::Future {
+        let mut svc = self.inner.clone();
+        let fut = async move {
+            let (parts, body) = request.into_parts();
+            let meta = metadata_from_headers(&parts.headers);
+            let input_stream = spawn_grpc_request_stream::<B, Input>(body);
+            let rpc_req = XlineRequest::new(input_stream, meta);
+
+            match svc.call(rpc_req).await {
+                Ok(resp) => {
+                    let framed = grpc_frame_encode(resp.data());
+                    Ok(grpc_ok_response(axum::body::Body::from(framed)))
+                }
+                Err(e) => Ok(grpc_error_response(e)),
+            }
+        };
+        Box::pin(fut)
     }
 }
